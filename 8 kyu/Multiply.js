9a76a4b0a1932263282ce2eb2474c9735ff775c3
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-04-25 14:43:15 +0100

    initial commit

---
+++ 8 kyu/Multiply.js
@@ -0,0 +1,19 @@
+/* Instructions:
+
+This code does not execute properly. Try to figure out why.
+
+Code: 
+function multiply(a, b){
+  a * b
+};
+
+*/
+
+//Solution:
+
+function multiply(a, b) {
+  let sum = a * b;
+  return sum;
+}
+
+multiply(1, 1);
