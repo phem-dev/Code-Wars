185d4313e099dcda771e51f767d22018a648c6b4
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-04-30 15:33:11 +0100

    initial commit

---
+++ 8 kyu/OppositeNumber.js
@@ -0,0 +1,16 @@
+/*Instructions:
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+//Solution:
+
+function opposite(number) {
+  return -number;
+}
