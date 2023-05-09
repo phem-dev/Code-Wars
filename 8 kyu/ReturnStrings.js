5dc9d5af9e511018bf3e8f0489e3de2c9425c85b
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-05-09 16:40:37 +0100

    initial commit

---
+++ 8 kyu/ReturnStrings.js
@@ -0,0 +1,9 @@
+/*Instructions:
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+
+//Solution:
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
