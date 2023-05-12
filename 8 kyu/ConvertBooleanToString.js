e67382cc735fe0a8cad578c5a6f95c462d486789
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-05-12 13:27:19 +0100

    initial commit

---
+++ 8 kyu/ConvertBooleanToString.js
@@ -0,0 +1,16 @@
+/*Instructions:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+//Solution:
+
+function booleanToString(b) {
+  if (b == true) {
+    return "true";
+  } else {
+    return "false";
+  }
+}
