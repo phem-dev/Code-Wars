684ea3596241820bef5852965e0f039c7c09d3df
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-05-21 16:51:11 +0100

    initial commit

---
+++ 8 kyu/MakeUpperCase.js
@@ -0,0 +1,11 @@
+/*Instructions:
+Write a function which converts the input string to uppercase.
+
+e.g "hello" -> "HELLO"
+
+*/
+
+//Solution:
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
