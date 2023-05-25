4c7d09c91d6639ac6e806eb15c876c6ba88d86ff
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-05-25 10:06:12 +0100

    initial commit

---
+++ 8 kyu/StringToArray.js
@@ -0,0 +1,13 @@
+/*Instructions:
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+
+//Solution:
+
+function stringToArray(string) {
+  let arr = string.split(" ");
+
+  return arr;
+}
