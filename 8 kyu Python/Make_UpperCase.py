25f425cf46c5f450fff654c0877d25b95223c454
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-06-26 12:30:28 +0100

    initial commit

---
+++ 8 kyu Python/Make_UpperCase.py
@@ -0,0 +1,14 @@
+"""
+Insructions:
+Write a function which converts the input string to uppercase.
+
+"""
+
+# Solution:
+
+
+def make_upper_case(s):
+    return s.upper()
+
+
+make_upper_case("hello")
