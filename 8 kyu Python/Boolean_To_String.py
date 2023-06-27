b8c89bb5955b3e37d26d9eb086a6a23d6db66a18
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-06-27 14:44:27 +0100

    initial commit

---
+++ 8 kyu Python/Boolean_To_String.py
@@ -0,0 +1,16 @@
+"""
+Instructions:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+"""
+
+# Solution:
+
+
+def boolean_to_string(b):
+    if b:
+        return "True"
+    else:
+        return "False"
