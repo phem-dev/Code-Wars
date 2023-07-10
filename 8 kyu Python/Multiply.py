e28a5bb82ebdba6ac9a8650af01e529b9d7f9a31
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-07-10 14:32:00 +0100

    initial commit

---
+++ 8 kyu Python/Multiply.py
@@ -0,0 +1,14 @@
+"""
+Instructions:
+This code does not execute properly. Try to figure out why.
+
+Before:
+def multiply(a, b):
+     a * b
+"""
+
+# Solution:
+
+
+def multiply(a, b):
+    return a * b
