ff71959dc432fb35bb99cb934ed202c87a1760e0
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-07-21 09:26:36 +0100

    initial commit

---
+++ 8 kyu Python/Find_The_Position.py
@@ -0,0 +1,15 @@
+"""
+Instructions:
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+"""
+
+# Solution:
+
+
+def position(alphabet):
+    return f"Position of alphabet: {ord(alphabet)-96}"
