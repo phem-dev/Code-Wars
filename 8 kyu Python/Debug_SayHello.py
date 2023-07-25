f8488167573b8df1edec3a0470a6d2add028182f
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-07-25 14:56:52 +0100

    initial commit

---
+++ 8 kyu Python/Debug_SayHello.py
@@ -0,0 +1,19 @@
+"""
+Instructions:
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+Before:
+def say_hello(name):
+'Hello'
+
+"""
+
+# Solution:
+
+
+def say_hello(name):
+    return f"Hello, {name}"
