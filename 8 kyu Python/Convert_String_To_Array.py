841971533706dac077ebd1f70d2eced758340323
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-07-27 13:42:51 +0100

    initial commit

---
+++ 8 kyu Python/Convert_String_To_Array.py
@@ -0,0 +1,12 @@
+"""
+Instructions:
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+"""
+
+
+# Solution:
+def string_to_array(s):
+    return s.split(" ")
