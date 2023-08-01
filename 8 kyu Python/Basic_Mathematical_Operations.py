ef7a56a0ab838de4524a12cd839c3cb8843e19bd
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-08-01 14:48:27 +0100

    initial commit

---
+++ 8 kyu Python/Basic_Mathematical_Operations.py
@@ -0,0 +1,20 @@
+"""
+Instructions:
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+"""
+
+
+# Solution:
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    else:
+        return value1 / value2
