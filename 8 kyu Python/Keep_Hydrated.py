0c59fc18f88f33414caff994a69a7fa4ba6133d3
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-08-14 13:46:48 +0100

    initial commit

---
+++ 8 kyu Python/Keep_Hydrated.py
@@ -0,0 +1,27 @@
+"""
+Instructions:
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+
+"""
+
+# Solution:
+import math
+
+
+def litres(time):
+    if time < 2:
+        return 0
+    else:
+        return math.floor(time * 0.5)
