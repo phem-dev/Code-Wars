109bf961edf41f746f833cbc02326c40843678e1
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-08-25 15:12:29 +0100

    Initial commit

---
+++ 8 kyu Python/Is_The_String_Uppercase.py
@@ -0,0 +1,21 @@
+"""
+Instructions:
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+"""
+
+
+# Solution:
+def is_uppercase(inp):
+    if inp == inp.upper():
+        return True
+    else:
+        return False
