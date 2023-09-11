0a47a1bd4b6e8d820e01d835d77fe2b6fee86d28
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-09-11 10:35:12 +0100

    initial commit

---
+++ 8 kyu Python/Return_To_Sanity.py
@@ -0,0 +1,10 @@
+"""
+Instructions:
+This function should return an object, but it's not doing what's intended. What's wrong?
+"""
+
+
+# Solution:
+def mystery():
+    results = {"sanity": "Hello"}
+    return results
