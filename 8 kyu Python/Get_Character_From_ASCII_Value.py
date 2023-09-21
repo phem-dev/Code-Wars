7bcb0d9b9b1cf70983c1f3737dd944eda1d2c92d
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-09-21 09:25:15 +0100

    initial commit

---
+++ 8 kyu Python/Get_Character_From_ASCII_Value.py
@@ -0,0 +1,15 @@
+"""
+Instructions:
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+"""
+
+
+# Solution:
+def get_char(c):
+    return chr(c)
