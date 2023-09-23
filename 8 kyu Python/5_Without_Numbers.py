7be8245e9436fe992d6b8ac2027d0c71ad1999e9
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-09-23 08:52:03 +0100

    initial commit

---
+++ 8 kyu Python/5_Without_Numbers.py
@@ -0,0 +1,11 @@
+"""
+Instructions:
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+"""
+
+
+# Solution:
+def unusual_five():
+    return int("5")
