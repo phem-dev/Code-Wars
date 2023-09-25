72d2405c04cb27770ffff0398cadae9fd477adad
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-09-25 09:54:40 +0100

    initial commit

---
+++ 8 kyu Python/Grasshopper_Check_For_Factor.py
@@ -0,0 +1,11 @@
+"""
+Instructions:
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+"""
+
+
+# Solution:
+def check_for_factor(base, factor):
+    return base % factor == 0
