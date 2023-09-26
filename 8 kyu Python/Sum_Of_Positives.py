0bc313d03eff7c14c8881669ea9c2268a5e1eb14
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-09-26 09:56:24 +0100

    initial commit

---
+++ 8 kyu Python/Sum_Of_Positives.py
@@ -0,0 +1,22 @@
+"""
+Instructions:
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+"""
+
+
+# Solution 1:
+def positive_sum(arr):
+    result = 0
+    for x in arr:
+        if x > 0:
+            result += x
+    return result
+
+
+# Solution 2 (refactored):
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
