4c8c3e502bfa185d3e46559cb35ed199caae6643
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-10-13 13:04:35 +0100

    initial commit

---
+++ 8 kyu Python/Reversed_Words.py
@@ -0,0 +1,13 @@
+"""
+Instructions:
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+"""
+
+
+# Solution:
+def reverse_words(s):
+    s.split()
+    word = s[6:-1] + "! " + s[0:5]
+    return word
