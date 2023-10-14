75d552dfe91d817b8abced19ecf8c5355ee36f50
Perrine <124287208+phem-dev@users.noreply.github.com>
2023-10-14 11:45:07 +0100

    initial commit

---
+++ 8 kyu Python/Bin_To_Decimal.py
@@ -0,0 +1,9 @@
+"""
+Instructions:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+"""
+
+
+# Solution:
+def bin_to_decimal(inp):
+    return int(inp, 2)
